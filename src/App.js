df2beb6d78e44bbeab1a54811c7ddbcb3300d006
czyzu05 <damian.czyz1998@gmail.com>
2021-02-18 16:03:50 +0100

    implement dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import {useState} from 'react'
+import Dropdown from "./components/Dropdown";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import { sliderData } from "./data/SliderData";
 import GlobalStyle from "./globalStyles";
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Navbar />
+      <Navbar toggle={toggle}/>
+      <Dropdown toggle={toggle} isOpen={isOpen} />
       <Hero slides={sliderData}/>
     </>
   );
